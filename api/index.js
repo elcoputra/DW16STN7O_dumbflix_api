f389888791b9bbb128168226b0cb2d3805bddfbd
Elcoputra <elcoputra94@gmail.com>
2020-06-11 07:26:02 +0700

    validator register beres

--- api/index.js
+++ api/index.js
@@ -6,6 +6,8 @@ const usersRoute = require('../controllers/user/users');
 const userRoute = require('../controllers/user/user');
 // const loginRoute = require('./usersData/login');
 const registerRoute = require('../controllers/user/register');
+const { validatingRegister } = require('./middlwares/validator');
+
 
 /* GET home page. */
 router.get('/', (req, res) => {
@@ -16,7 +18,7 @@ router.get('/', (req, res) => {
 router.get('/users', usersRoute.reads);
 router.get('/user/:id', userRoute.read);
 // router.get('/login/:id', loginRoute.check);
-router.post('/register', registerRoute.create);
+router.post('/register', validatingRegister, registerRoute.create);
 
 
 module.exports = router;
---
+++ api/middlwares/validator.js
@@ -0,0 +1,16 @@
+const Joi = require('@hapi/joi');
+
+exports.validatingRegister = (req, res, next) => {
+  const schema = Joi.object({
+    fullName: Joi.string().min(2).required(),
+    email: Joi.string().email().min(13).required(),
+    password: Joi.string().min(8).max(16).required(),
+    gender: Joi.allow().required(),
+    phone: Joi.string().min(10).required(),
+    address: Joi.allow().required(),
+    subscribe: Joi.boolean().required(),
+  });
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(400).json({ error: error.details[0].message });
+  next();
+};
